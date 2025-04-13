8c31e5c5db12b717587a76cbb03b86e09f847d64
Chuka Ogbodo <chukaogbodo7@gmail.com>
2025-04-13 22:18:39 +0100

    Added GameService Class

---
+++ GameStore.Application/Services/GameService.cs
@@ -0,0 +1,21 @@
+﻿using GameStore.Domain.Entities;
+using GameStore.Domain.Interfaces;
+
+namespace GameStore.Application.Services
+{
+    public class GameService
+    {
+        private readonly IGameRepository _gameRepository;
+
+        public GameService(IGameRepository gameRepository)
+        {
+            _gameRepository = gameRepository;
+        }
+
+        public Task<List<Game>> GetAllAsync() => _gameRepository.GetAllAsync();
+        public Task<Game?> GetByIdAsync(Guid id) => _gameRepository.GetByIdAsync(id);
+        public Task AddAsync(Game game) => _gameRepository.AddAsync(game);
+        public Task UpdateAsync(Game game) => _gameRepository.UpdateAsync(game);
+        public Task DeleteAsync(Guid id) => _gameRepository.DeleteAsync(id);
+    }
+}
